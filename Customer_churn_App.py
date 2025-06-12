75554f0ce913c34499eb73fdaa99058d0e27ec44
Hamshary1000 <157809997+Hamshary1000@users.noreply.github.com>
2025-06-12 09:29:39 -0700

    Update Customer_churn_App.py

--- Customer_churn_App.py
+++ Customer_churn_App.py
@@ -41,12 +41,14 @@ with tab2:
         <iframe 
             title="Customer Churn Dashboard" 
             width="100%" 
-            height="740" 
+            height="650" 
             src="https://app.powerbi.com/view?r=eyJrIjoiZmEwMDBmZDctYTU2Yy00OWUxLTk0ZDktMWEyZmUxMjU5MTNjIiwidCI6IjJiYjZlNWJjLWMxMDktNDdmYi05NDMzLWMxYzZmNGZhMzNmZiIsImMiOjl9" 
             frameborder="0" 
+            style="border:none; overflow:hidden;" 
             allowFullScreen="true">
         </iframe>
     """, unsafe_allow_html=True)
+
 
 
 # ---------------------
